19883d63d0a3d7678ed9093445cbca014e35f155
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-05-21 12:13:43 +0200

    Added terraform script

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+# Setup aws provider
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.42"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
