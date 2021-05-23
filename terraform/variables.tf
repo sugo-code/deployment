c9113f8b4f8699a2117741a2560e1c74a96dfb9b
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-05-23 11:06:46 +0200

    updated variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,6 +10,8 @@ variable "prefix" {
   default = "clod2021-group2"
 }
 
+# The School's AWS account has already assigned 5 elastic ips, which is the maximum amount.
+# Instead of creating a new elastic ip, an already existing one will be used
 variable "elastic_ip_id" {
   type = string
   description = "AWS elastic ip id, used in the NAT Gateway, to allow machines in the private subnet to access the internet"
