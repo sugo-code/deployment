3713bd831c760f310df44140438b22aaa9c4a426
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-05-25 13:58:42 +0200

    updated deployment.tf

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,32 +1,9 @@
-# CodeDeploy when used with autoscaling groups based on launch templates needs additional permissions
-
-# The school's account still has to add iam:DeletePolicy permission, for the time being an already existing one will be used
-
-# resource "aws_iam_policy" "main" {
-#   name = "${var.prefix}-codedeploy-role-policy"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "ec2:RunInstances",
-#           "ec2:CreateTags",
-#           "iam:PassRole"
-#         ]
-#         Resource = "*"
-#       }
-#     ]
-#   })
-# }
-
-# Create a CodeDeploy service role
+# Create service roles
 
 resource "aws_iam_role" "codebuild" {
-   name = "${var.prefix}-codebuild-role"
+  name = "${var.prefix}-codebuild-role"
 
-   assume_role_policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
@@ -44,7 +21,6 @@ resource "aws_iam_role" "codedeploy" {
   name = "${var.prefix}-codedeploy-role"
 
   managed_policy_arns = [
-    # Not needed with no autoscaling group "arn:aws:iam::240595528763:policy/clod2021-group2-codedeploy-role-policy", # TODO: replace with aws_iam_policy var when iam:DeletePolicy permission is added
     "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
   ]
 
@@ -63,13 +39,31 @@ resource "aws_iam_role" "codedeploy" {
       }
     ]
   })
+
+  inline_policy {
+    name = "${var.prefix}-codedeploy-autoscaling-group-additional-permissions-policy"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = [
+            "ec2:RunInstances",
+            "ec2:CreateTags",
+            "iam:PassRole"
+          ]
+          Resource = "*"
+        }
+      ]
+    })
+  } 
 }
 
 # Create the deployment apps
 
-resource "aws_codedeploy_app" "auth_api" {
-  name = "${var.prefix}-auth-api"
-}
+# resource "aws_codedeploy_app" "auth_api" {
+#   name = "${var.prefix}-auth-api"
+# }
 
 # resource "aws_codedeploy_app" "data_api" {
 #   name = "${var.prefix}-data-api"
@@ -83,25 +77,25 @@ resource "aws_codedeploy_app" "auth_api" {
 #   name = "${var.prefix}-alarms-api"
 # }
 
-# resource "aws_codedeploy_app" "api_gateway" {
-#   name = "${var.prefix}-api-gateway"
-# }
+resource "aws_codedeploy_app" "api_gateway" {
+  name = "${var.prefix}-api-gateway"
+}
 
 # Create the deployment groups
 
-resource "aws_codedeploy_deployment_group" "auth_api" {
-  app_name = aws_codedeploy_app.auth_api.name
-  deployment_group_name = "${var.prefix}-deployment-group-auth-api"
-  service_role_arn = aws_iam_role.codedeploy.arn
+# resource "aws_codedeploy_deployment_group" "auth_api" {
+#   app_name = aws_codedeploy_app.auth_api.name
+#   deployment_group_name = "${var.prefix}-deployment-group-auth-api"
+#   service_role_arn = aws_iam_role.codedeploy.arn
 
-  ec2_tag_filter {
-    key = "Name"
-    type = "KEY_AND_VALUE"
-    value = "${var.prefix}-auth-api"
-  }
+#   ec2_tag_filter {
+#     key = "Name"
+#     type = "KEY_AND_VALUE"
+#     value = "${var.prefix}-auth-api"
+#   }
 
-  #autoscaling_groups = [aws_autoscaling_group.auth_api.id]
-}
+#   #autoscaling_groups = [aws_autoscaling_group.auth_api.id]
+# }
 
 # resource "aws_codedeploy_deployment_group" "data_api" {
 #   app_name = aws_codedeploy_app.data_api.name
@@ -139,14 +133,94 @@ resource "aws_codedeploy_deployment_group" "auth_api" {
 #   }
 # }
 
-# resource "aws_codedeploy_deployment_group" "api_gateway" {
-#   app_name = aws_codedeploy_app.api_gateway.name
-#   deployment_group_name = "${var.prefix}-deployment-group-api-gateway"
-#   service_role_arn = aws_iam_role.codedeploy.arn
+resource "aws_codedeploy_deployment_group" "api_gateway" {
+  app_name = aws_codedeploy_app.api_gateway.name
+  deployment_group_name = "${var.prefix}-deployment-group-api-gateway"
+  service_role_arn = aws_iam_role.codedeploy.arn
 
-#   ec2_tag_filter {
-#     key = "Name"
-#     type = "KEY_AND_VALUE"
-#     value = "${var.prefix}-alarms-api"
-#   }
-# }
+  ec2_tag_filter {
+    key = "Name"
+    type = "KEY_AND_VALUE"
+    value = "${var.prefix}-api-gateway"
+  }
+}
+
+resource "aws_s3_bucket" "secrets" {
+  bucket = "${var.prefix}-bucket-secrets"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_object" "api_gateway_secrets" {
+  key    = "api-gateway/.env"
+  bucket = aws_s3_bucket.secrets.id
+ 
+  content = <<EOT
+    DATA_API_URL=http://test
+    AUTH_API_URL=http://test2
+    REALTIME_API_URL=http://test3
+    EOT
+
+  # content = <<EOT
+  #   DATA_API_URL=http://${aws_instance.data_api.private_dns}
+  #   AUTH_API_URL=http://${aws_instance.auth_api.private_dns}
+  #   REALTIME_API_URL=http://${aws_instance.realtime_api.private_dns}
+  #   EOT
+}
+
+resource "aws_s3_bucket_object" "realtime_api_secrets" {
+  key    = "realtime-api/.env"
+  bucket = aws_s3_bucket.secrets.id
+ 
+  #TODO: Add env vars
+  content = <<EOT
+    RABBITMQ_URL=ampqs://
+    EOT
+}
+
+resource "aws_s3_bucket_object" "auth_api_secrets" {
+  key    = "auth-api/.env"
+  bucket = aws_s3_bucket.secrets.id
+ 
+  #TODO: Add env vars
+  content = <<EOT
+    MONGO_URI=
+    MONGO_DB=
+    RABBITMQ_URL=
+    JWT_SECRET=
+    EOT
+}
+
+resource "aws_s3_bucket_object" "alarms_api_secrets" {
+  key    = "alarms-api/.env"
+  bucket = aws_s3_bucket.secrets.id
+ 
+  #TODO: Add env vars
+  content = <<EOT
+    VONAGE_API_KEY=
+    VONAGE_API_SECRET=
+    RABBITMQ_URL=
+    EOT
+}
+
+resource "aws_s3_bucket_object" "data_api_secrets" {
+  key    = "data-api/.env"
+  bucket = aws_s3_bucket.secrets.id
+ 
+  #TODO: Add env vars
+  content = <<EOT
+    POSTGRESQL_URL=
+    INFLUXDB_URL=
+    INFLUXDB_ORG=
+    INFLUXDB_TOKEN=
+    INFLUXDB_BUCKET=
+    RABBITMQ_URL=
+    EOT
+}
