d226f6178fd48cc48f83ee1a1706f8f0ee0ab065
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-05-26 22:28:26 +0200

    updated env vars

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,3 +1,11 @@
+locals {
+  rabbitmq_host = regex("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?", aws_mq_broker.message_bus.instances.0.endpoints.0).3
+  
+  rabbitmq_url = "amqps://${var.rabbitmq_username}:${var.rabbitmq_password}@${local.rabbitmq_host}"
+  postgresql_url = "postgres://${var.postgresql_username}:${var.postgresql_password}@${aws_db_instance.parameters_db.endpoint}/${var.postgresql_database}"
+  mongodb_url = "mongodb://${var.mongodb_username}:${var.mongodb_password}@${aws_docdb_cluster_instance.auth_db.0.endpoint}:27017/${var.mongodb_database}?retryWrites=false"
+}
+
 # Create service roles
 
 resource "aws_iam_role" "codebuild" {
@@ -61,21 +69,21 @@ resource "aws_iam_role" "codedeploy" {
 
 # Create the deployment apps
 
-# resource "aws_codedeploy_app" "auth_api" {
-#   name = "${var.prefix}-auth-api"
-# }
+resource "aws_codedeploy_app" "auth_api" {
+  name = "${var.prefix}-auth-api"
+}
 
-# resource "aws_codedeploy_app" "data_api" {
-#   name = "${var.prefix}-data-api"
-# }
+resource "aws_codedeploy_app" "data_api" {
+  name = "${var.prefix}-data-api"
+}
 
-# resource "aws_codedeploy_app" "realtime_api" {
-#   name = "${var.prefix}-realtime-api"
-# }
+resource "aws_codedeploy_app" "realtime_api" {
+  name = "${var.prefix}-realtime-api"
+}
 
-# resource "aws_codedeploy_app" "alarms_api" {
-#   name = "${var.prefix}-alarms-api"
-# }
+resource "aws_codedeploy_app" "alarms_api" {
+  name = "${var.prefix}-alarms-api"
+}
 
 resource "aws_codedeploy_app" "api_gateway" {
   name = "${var.prefix}-api-gateway"
@@ -83,55 +91,55 @@ resource "aws_codedeploy_app" "api_gateway" {
 
 # Create the deployment groups
 
-# resource "aws_codedeploy_deployment_group" "auth_api" {
-#   app_name = aws_codedeploy_app.auth_api.name
-#   deployment_group_name = "${var.prefix}-deployment-group-auth-api"
-#   service_role_arn = aws_iam_role.codedeploy.arn
+resource "aws_codedeploy_deployment_group" "auth_api" {
+  app_name = aws_codedeploy_app.auth_api.name
+  deployment_group_name = "${var.prefix}-deployment-group-auth-api"
+  service_role_arn = aws_iam_role.codedeploy.arn
 
-#   ec2_tag_filter {
-#     key = "Name"
-#     type = "KEY_AND_VALUE"
-#     value = "${var.prefix}-auth-api"
-#   }
+  ec2_tag_filter {
+    key = "Name"
+    type = "KEY_AND_VALUE"
+    value = "${var.prefix}-auth-api"
+  }
 
-#   #autoscaling_groups = [aws_autoscaling_group.auth_api.id]
-# }
+  #autoscaling_groups = [aws_autoscaling_group.auth_api.id]
+}
 
-# resource "aws_codedeploy_deployment_group" "data_api" {
-#   app_name = aws_codedeploy_app.data_api.name
-#   deployment_group_name = "${var.prefix}-deployment-group-data-api"
-#   service_role_arn = aws_iam_role.codedeploy.arn
+resource "aws_codedeploy_deployment_group" "data_api" {
+  app_name = aws_codedeploy_app.data_api.name
+  deployment_group_name = "${var.prefix}-deployment-group-data-api"
+  service_role_arn = aws_iam_role.codedeploy.arn
 
-#   ec2_tag_filter {
-#     key = "Name"
-#     type = "KEY_AND_VALUE"
-#     value = "${var.prefix}-data-api"
-#   }
-# }
+  ec2_tag_filter {
+    key = "Name"
+    type = "KEY_AND_VALUE"
+    value = "${var.prefix}-data-api"
+  }
+}
 
-# resource "aws_codedeploy_deployment_group" "realtime_api" {
-#   app_name = aws_codedeploy_app.realtime_api.name
-#   deployment_group_name = "${var.prefix}-deployment-group-realtime-api"
-#   service_role_arn = aws_iam_role.codedeploy.arn
+resource "aws_codedeploy_deployment_group" "realtime_api" {
+  app_name = aws_codedeploy_app.realtime_api.name
+  deployment_group_name = "${var.prefix}-deployment-group-realtime-api"
+  service_role_arn = aws_iam_role.codedeploy.arn
 
-#   ec2_tag_filter {
-#     key = "Name"
-#     type = "KEY_AND_VALUE"
-#     value = "${var.prefix}-realtime-api"
-#   }
-# }
+  ec2_tag_filter {
+    key = "Name"
+    type = "KEY_AND_VALUE"
+    value = "${var.prefix}-realtime-api"
+  }
+}
 
-# resource "aws_codedeploy_deployment_group" "alarms_api" {
-#   app_name = aws_codedeploy_app.alarms_api.name
-#   deployment_group_name = "${var.prefix}-deployment-group-alarms-api"
-#   service_role_arn = aws_iam_role.codedeploy.arn
+resource "aws_codedeploy_deployment_group" "alarms_api" {
+  app_name = aws_codedeploy_app.alarms_api.name
+  deployment_group_name = "${var.prefix}-deployment-group-alarms-api"
+  service_role_arn = aws_iam_role.codedeploy.arn
 
-#   ec2_tag_filter {
-#     key = "Name"
-#     type = "KEY_AND_VALUE"
-#     value = "${var.prefix}-alarms-api"
-#   }
-# }
+  ec2_tag_filter {
+    key = "Name"
+    type = "KEY_AND_VALUE"
+    value = "${var.prefix}-alarms-api"
+  }
+}
 
 resource "aws_codedeploy_deployment_group" "api_gateway" {
   app_name = aws_codedeploy_app.api_gateway.name
@@ -163,25 +171,18 @@ resource "aws_s3_bucket_object" "api_gateway_secrets" {
   bucket = aws_s3_bucket.secrets.id
  
   content = <<EOT
-    DATA_API_URL=http://test
-    AUTH_API_URL=http://test2
-    REALTIME_API_URL=http://test3
+    DATA_API_URL=http://${aws_instance.data_api.private_dns}
+    AUTH_API_URL=http://${aws_instance.auth_api.private_dns}
+    REALTIME_API_URL=http://${aws_instance.realtime_api.private_dns}
     EOT
-
-  # content = <<EOT
-  #   DATA_API_URL=http://${aws_instance.data_api.private_dns}
-  #   AUTH_API_URL=http://${aws_instance.auth_api.private_dns}
-  #   REALTIME_API_URL=http://${aws_instance.realtime_api.private_dns}
-  #   EOT
 }
 
 resource "aws_s3_bucket_object" "realtime_api_secrets" {
   key    = "realtime-api/.env"
   bucket = aws_s3_bucket.secrets.id
  
-  #TODO: Add env vars
   content = <<EOT
-    RABBITMQ_URL=ampqs://
+    RABBITMQ_URL=${local.rabbitmq_url}
     EOT
 }
 
@@ -189,12 +190,11 @@ resource "aws_s3_bucket_object" "auth_api_secrets" {
   key    = "auth-api/.env"
   bucket = aws_s3_bucket.secrets.id
  
-  #TODO: Add env vars
   content = <<EOT
-    MONGO_URI=
-    MONGO_DB=
-    RABBITMQ_URL=
-    JWT_SECRET=
+    MONGO_URI=${local.mongodb_url}
+    MONGO_DB=${var.mongodb_database}
+    RABBITMQ_URL=${local.rabbitmq_url}
+    JWT_SECRET=${var.jwt_encryption_secret}
     EOT
 }
 
@@ -202,11 +202,10 @@ resource "aws_s3_bucket_object" "alarms_api_secrets" {
   key    = "alarms-api/.env"
   bucket = aws_s3_bucket.secrets.id
  
-  #TODO: Add env vars
   content = <<EOT
-    VONAGE_API_KEY=
-    VONAGE_API_SECRET=
-    RABBITMQ_URL=
+    VONAGE_API_KEY=${var.vonage_api_key}
+    VONAGE_API_SECRET=${var.vonage_api_secret}
+    RABBITMQ_URL=${local.rabbitmq_url}
     EOT
 }
 
@@ -214,13 +213,12 @@ resource "aws_s3_bucket_object" "data_api_secrets" {
   key    = "data-api/.env"
   bucket = aws_s3_bucket.secrets.id
  
-  #TODO: Add env vars
   content = <<EOT
-    POSTGRESQL_URL=
-    INFLUXDB_URL=
-    INFLUXDB_ORG=
-    INFLUXDB_TOKEN=
-    INFLUXDB_BUCKET=
-    RABBITMQ_URL=
+    POSTGRESQL_URL=${local.postgresql_url}
+    INFLUXDB_URL=http://${aws_instance.data_db.private_dns}:8086
+    INFLUXDB_ORG=${var.influxdb_organization}
+    INFLUXDB_TOKEN=${var.influxdb_token}
+    INFLUXDB_BUCKET=${var.influxdb_bucket}
+    RABBITMQ_URL=${local.rabbitmq_url}
     EOT
 }
