6fd93777d1b9b0c1eac3e2ae4e42801ea77f1f10
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-05-26 22:29:16 +0200

    added all microservices

--- terraform/microservices.tf
+++ terraform/microservices.tf
@@ -16,6 +16,59 @@
 # }
 
 # Frontend
+# TODO: Guess what it's not working
+resource "aws_cloudfront_distribution" "web_app" {
+  origin {
+    domain_name = "${aws_s3_bucket.web_app.website_endpoint}"
+    origin_id = "${aws_s3_bucket.web_app.bucket}"
+
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled = true
+  default_root_object = "index.html"
+
+  custom_error_response {
+    error_code    = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.web_app.bucket
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
 
 resource "aws_s3_bucket" "web_app" {
   bucket = "${var.prefix}-bucket-web-app"
@@ -40,60 +93,76 @@ resource "aws_s3_bucket" "web_app" {
 
   website {
     index_document = "index.html"
+    error_document = "index.html"
   }
+
+  force_destroy = true
 }
 
 # APIs
+resource "aws_instance" "auth_api" {
+  ami = aws_launch_template.docker.image_id
+  instance_type = aws_launch_template.docker.instance_type
 
-# resource "aws_instance" "auth_api" {
-#   ami = aws_launch_template.nodejs.image_id
-#   instance_type = aws_launch_template.nodejs.instance_type
+  vpc_security_group_ids = aws_launch_template.docker.vpc_security_group_ids
+  subnet_id = aws_subnet.private.id
 
-#   security_groups = aws_launch_template.nodejs.security_group_names
-#   subnet_id = aws_subnet.private.id
+  iam_instance_profile = aws_launch_template.docker.iam_instance_profile[0].name
 
-#   tags = {
-#     Name = "${var.prefix}-auth-api"
-#   }
-# }
+  key_name = var.ssh_key_name
 
-# resource "aws_instance" "data_api" {
-#   ami = aws_launch_template.nodejs.image_id
-#   instance_type = aws_launch_template.nodejs.instance_type
+  tags = {
+    Name = "${var.prefix}-auth-api"
+  }
+}
 
-#   security_groups = aws_launch_template.nodejs.security_group_names
-#   subnet_id = aws_subnet.private.id
+resource "aws_instance" "data_api" {
+  ami = aws_launch_template.docker.image_id
+  instance_type = aws_launch_template.docker.instance_type
 
-#   tags = {
-#     Name = "${var.prefix}-data-api"
-#   }
-# }
+  vpc_security_group_ids = aws_launch_template.docker.vpc_security_group_ids
+  subnet_id = aws_subnet.private.id
 
-# resource "aws_instance" "realtime_api" {
-#   ami = aws_launch_template.nodejs.image_id
-#   instance_type = aws_launch_template.nodejs.instance_type
+  iam_instance_profile = aws_launch_template.docker.iam_instance_profile[0].name
 
-#   security_groups = aws_launch_template.nodejs.security_group_names
-#   subnet_id = aws_subnet.private.id
+  key_name = var.ssh_key_name
 
-#   tags = {
-#     Name = "${var.prefix}-realtime-api"
-#   }
-# }
+  tags = {
+    Name = "${var.prefix}-data-api"
+  }
+}
 
-# resource "aws_instance" "alarms_api" {
-#   ami = aws_launch_template.nodejs.image_id
-#   instance_type = aws_launch_template.nodejs.instance_type
+resource "aws_instance" "alarms_api" {
+  ami = aws_launch_template.docker.image_id
+  instance_type = aws_launch_template.docker.instance_type
 
-#   security_groups = aws_launch_template.nodejs.security_group_names
-#   subnet_id = aws_subnet.private.id
+  vpc_security_group_ids = aws_launch_template.docker.vpc_security_group_ids
+  subnet_id = aws_subnet.private.id
 
-#   tags = {
-#     Name = "${var.prefix}-alarms-api"
-#   }
-# }
+  iam_instance_profile = aws_launch_template.docker.iam_instance_profile[0].name
 
-# Gateway
+  key_name = var.ssh_key_name
+
+  tags = {
+    Name = "${var.prefix}-alarms-api"
+  }
+}
+
+resource "aws_instance" "realtime_api" {
+  ami = aws_launch_template.docker.image_id
+  instance_type = aws_launch_template.docker.instance_type
+
+  vpc_security_group_ids = aws_launch_template.docker.vpc_security_group_ids
+  subnet_id = aws_subnet.private.id
+
+  iam_instance_profile = aws_launch_template.docker.iam_instance_profile[0].name
+
+  key_name = var.ssh_key_name
+
+  tags = {
+    Name = "${var.prefix}-realtime-api"
+  }
+}
 
 resource "aws_instance" "api_gateway" {
   ami = aws_launch_template.docker.image_id
@@ -139,7 +208,7 @@ resource "aws_instance" "data_db" {
   instance_type = aws_launch_template.influxdb.instance_type
 
   vpc_security_group_ids = aws_launch_template.influxdb.vpc_security_group_ids
-  subnet_id = aws_subnet.public.id #TODO: Change to private
+  subnet_id = aws_subnet.private.id
 
   key_name = var.ssh_key_name 
   tags = {
@@ -182,13 +251,25 @@ resource "aws_docdb_subnet_group" "main" {
   subnet_ids = [aws_subnet.private.id, aws_subnet.private_2.id, aws_subnet.private_3.id]
 }
 
+resource "aws_docdb_cluster_parameter_group" "main" {
+  family      = "docdb3.6"
+  name        = "${var.prefix}-auth-db-parameter-group"
+
+  parameter {
+    name  = "tls"
+    value = "disabled"
+  }
+}
+
 resource "aws_docdb_cluster" "auth_db" {
   cluster_identifier  = "${var.prefix}-auth-db"
   engine              = "docdb"
+  engine_version      = "3.6.0"
 
   master_username     = var.mongodb_username
   master_password     = var.mongodb_password
 
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
   db_subnet_group_name = aws_docdb_subnet_group.main.name
   vpc_security_group_ids = [
     aws_security_group.mongodb.id, 
@@ -198,7 +279,7 @@ resource "aws_docdb_cluster" "auth_db" {
   skip_final_snapshot = true
 }
 
-resource "aws_docdb_cluster_instance" "main" {
+resource "aws_docdb_cluster_instance" "auth_db" {
   count              = 1
   identifier         = "${var.prefix}-auth-db"
   cluster_identifier = aws_docdb_cluster.auth_db.id
